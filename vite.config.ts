0cd9afbbd09d75e954535263f862bf450d044bcd
gBelfiore <belfiore.giovanni@gmail.com>
2024-10-31 09:54:17 +0100

    chore: fix path lib

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,9 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@/components': path.resolve(__dirname, './src/components'),
+      '@/utils': path.resolve(__dirname, './src/utils'),
+      '@/zustand': path.resolve(__dirname, './src/zustand'),
     },
   },
   build: {
