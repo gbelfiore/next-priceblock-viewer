eefbbdf5252cf947e27c80ab8778799ba33365b5
gBelfiore <belfiore.giovanni@gmail.com>
2025-03-04 18:26:21 +0100

    test 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import path from 'path';
+// import path from 'path';
 import dts from 'vite-plugin-dts';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
